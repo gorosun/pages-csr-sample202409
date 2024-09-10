1390b9d8e89f83ca6f1d60e3adb96c8d18cf1b8f
gorosun <goro-hayakawa@no-studio.net>
2024-09-10 15:32:27 +0900

    Add import alias configuration

--- member-site/vite.config.ts
+++ member-site/vite.config.ts
@@ -1,8 +1,12 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths() // TypeScriptのエイリアスをサポート
+  ],
   build: {
     sourcemap: false, // ソースマップの生成を無効化
   }
