775ffde5d0760287bdec44d6a96b736a40bdb64c
gorosun <goro-hayakawa@no-studio.net>
2024-09-10 16:03:36 +0900

    Add test configuration

--- member-site/vite.config.ts
+++ member-site/vite.config.ts
@@ -1,13 +1,19 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths() // TypeScriptのエイリアスをサポート
-  ],
+  plugins: [react(), tsconfigPaths()],
   build: {
     sourcemap: false, // ソースマップの生成を無効化
-  }
-})
+  },
+  test: {
+    globals: true, // グローバルにテストユーティリティを提供
+    environment: 'happy-dom', // 仮想DOMを使用することでブラウザの動作をエミュレート
+    coverage: {
+      provider: 'v8', // V8エンジンを使用したカバレッジの生成
+      reporter: ['text', 'json', 'html'], // 生成するレポートのフォーマット
+    },
+  },
+});
